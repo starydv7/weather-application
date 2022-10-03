a6fcc64aecf9a3ebf2d3e88a604aab4ab169afe0
starydv7 <pawanstar72926@gmail.com>
2022-10-03 23:40:22 +0530

    complete

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import HourlyChart from '../Chart/HourlyChart';
+
 import DailyForeCast from '../DailyForecast/DaillyForeCast';
 import "./Dashboard.css";
 import {arr} from "../constant"
